6493ed585f258cc0942c1c0d80b5dcad654de2e9
takeoischifan <takeoischifan@github.com>
2025-10-13 11:30:50 +0200

    docs: some more comments, formatting

--- raylib_libmpv_video_basic_example.c
+++ raylib_libmpv_video_basic_example.c
@@ -5,101 +5,119 @@
 #include "rlgl.h"
 #include <stdio.h>
 
-static void on_mpv_render_updates(void *ctx) { (void)ctx; }
+/*
+    In this basic example, we setup the boilerplate required to draw a video in a Raylib RenderTexture.
+*/
 
-static void on_mpv_events(void *ctx) {}
+/* Unused */
+static void on_mpv_render_updates(void* ctx) { (void)ctx; }
+static void on_mpv_events(void* ctx) {}
 
-static void *get_mpv_gl_proc_address(void *ctx, const char *name) {
-  (void)ctx;
-  return (void *)rlGetProcAddress(name);
+/* Tell MPV where to find OpenGL functions */
+static void* get_mpv_gl_proc_address(void* ctx, const char* name) {
+    (void)ctx;
+    return (void*)rlGetProcAddress(name);
 }
 
 int main(void) {
-  const char *videoPath = "./test.mp4";
-  const int screenWidth = 1280;
-  const int screenHeight = 720;
 
-  InitWindow(screenWidth, screenHeight, "Raylib + MPV");
-  rlDisableBackfaceCulling();
+    const char* videoPath = "./test.mp4";
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-  RenderTexture2D mpv_tex = LoadRenderTexture(screenWidth, screenHeight);
+    InitWindow(screenWidth, screenHeight, "Raylib + MPV");
 
-  mpv_handle *mpv_ctx = mpv_create();
-  if (!mpv_ctx) {
-    fprintf(stderr, "Could not create libmpv wrapper, Bailing.\n");
-    CloseWindow();
-    return -1;
-  }
+    /* IMPORTANT! Raylib will not render correctly unless you disable this */
+    rlDisableBackfaceCulling();
 
-  mpv_set_option_string(mpv_ctx, "vo", "libmpv");
-  mpv_set_option_string(mpv_ctx, "hwdec", "auto");
-  // if you want more debugging
-  // mpv_set_option_string(mpv_ctx, "msg-level", "all=v");
-  // mpv_set_option_string(mpv_ctx, "terminal", "yes");
+    /* Create GPU framebuffer for MPV to render into */
+    RenderTexture2D mpv_tex = LoadRenderTexture(screenWidth, screenHeight);
 
-  int error = mpv_initialize(mpv_ctx);
-  if (error != MPV_ERROR_SUCCESS) {
-    fprintf(stderr, "Could not initialize libmpv wrapper: %s, Bailing.\n",
-            mpv_error_string(error));
-    mpv_destroy(mpv_ctx);
-    CloseWindow();
-    return -1;
-  }
+    /* Create MPV context */
+    mpv_handle* mpv_ctx = mpv_create();
+    if (!mpv_ctx) {
+        fprintf(stderr, "Could not create libmpv wrapper, Bailing.\n");
+        CloseWindow();
+        return -1;
+    }
 
-  mpv_opengl_init_params gl_init = {
-      .get_proc_address = get_mpv_gl_proc_address,
-  };
+    mpv_set_option_string(mpv_ctx, "vo", "libmpv");  // required
+    mpv_set_option_string(mpv_ctx, "hwdec", "auto"); // set "no" for software rendering
+    // if you want more debugging
+    // mpv_set_option_string(mpv_ctx, "msg-level", "all=v");
+    // mpv_set_option_string(mpv_ctx, "terminal", "yes");
 
-  mpv_render_param params[] = {
-      {MPV_RENDER_PARAM_API_TYPE, MPV_RENDER_API_TYPE_OPENGL},
-      {MPV_RENDER_PARAM_OPENGL_INIT_PARAMS, &gl_init},
-      {MPV_RENDER_PARAM_ADVANCED_CONTROL, &(int){0}},
-      {0}};
+    /* Initialize MPV context */
+    int error = mpv_initialize(mpv_ctx);
+    if (error != MPV_ERROR_SUCCESS) {
+        fprintf(stderr, "Could not initialize libmpv wrapper: %s, Bailing.\n", mpv_error_string(error));
+        mpv_destroy(mpv_ctx);
+        CloseWindow();
+        return -1;
+    }
 
-  mpv_render_context *mpv_render_ctx = NULL;
-  error = mpv_render_context_create(&mpv_render_ctx, mpv_ctx, params);
-  if (error != MPV_ERROR_SUCCESS) {
-    fprintf(stderr, "Could not initialize libmpv render context: %s, Bailing.",
-            mpv_error_string(error));
-    mpv_destroy(mpv_ctx);
-    mpv_render_context_free(mpv_render_ctx);
-    CloseWindow();
-    return -1;
-  }
-
-  mpv_render_context_set_update_callback(mpv_render_ctx, on_mpv_render_updates,
-                                         NULL);
-  mpv_set_wakeup_callback(mpv_ctx, on_mpv_events, NULL);
-
-  const char *cmd[] = {"loadfile", videoPath, NULL};
-  mpv_command_async(mpv_ctx, 0, cmd);
-
-  while (!WindowShouldClose()) {
-
-    mpv_render_context_update(mpv_render_ctx);
-
-    mpv_opengl_fbo mpv_fbo = {
-        .fbo = mpv_tex.id,
-        .w = screenWidth,
-        .h = screenHeight,
+    /* Initialize MPV OpenGL rendering context */
+    mpv_opengl_init_params gl_init = {
+        .get_proc_address = get_mpv_gl_proc_address, // callback we defined earlier
     };
-    int flip_y = 1;
-    mpv_render_param render_params[] = {{MPV_RENDER_PARAM_OPENGL_FBO, &mpv_fbo},
-                                        {MPV_RENDER_PARAM_FLIP_Y, &flip_y},
-                                        {0}};
 
-    mpv_render_context_render(mpv_render_ctx, render_params);
+    mpv_render_param params[] = {
+        {MPV_RENDER_PARAM_API_TYPE, MPV_RENDER_API_TYPE_OPENGL},
+        {MPV_RENDER_PARAM_OPENGL_INIT_PARAMS, &gl_init},
+        {MPV_RENDER_PARAM_ADVANCED_CONTROL, &(int){0}}, // See the advanced example for advanced control.
+        {0}};
 
-    BeginDrawing();
-    DrawTextureRec(mpv_tex.texture,
-                   (Rectangle){0.f, 0.f, screenWidth, -1.f * screenHeight},
-                   (Vector2){0, 0}, WHITE);
-    ClearBackground(RAYWHITE);
-    EndDrawing();
-  }
+    mpv_render_context* mpv_render_ctx = NULL;
+    error = mpv_render_context_create(&mpv_render_ctx, mpv_ctx, params);
+    if (error != MPV_ERROR_SUCCESS) {
+        fprintf(stderr, "Could not initialize libmpv render context: %s, Bailing.", mpv_error_string(error));
+        mpv_render_context_free(mpv_render_ctx);
+        mpv_destroy(mpv_ctx);
+        CloseWindow();
+        return -1;
+    }
 
-  mpv_render_context_free(mpv_render_ctx);
-  mpv_destroy(mpv_ctx);
-  CloseWindow();
-  return 0;
+    /* Set MPV and render callbacks. We will not use these in this basic example, but setting them is recommended as the
+     * event queues can fill if you do not flush them, leading to unexpected behavior. */
+    mpv_render_context_set_update_callback(mpv_render_ctx, on_mpv_render_updates, NULL);
+    mpv_set_wakeup_callback(mpv_ctx, on_mpv_events, NULL);
+
+    /* Load video file */
+    const char* cmd[] = {"loadfile", videoPath, NULL};
+    mpv_command(mpv_ctx, cmd);
+
+    while (!WindowShouldClose()) {
+
+        /*
+            Render the MPV video into the buffer we created earlier.
+            If you just want to render in the default Raylib window, you an skip creating a RenderTexture and just pass
+           .fbo = 0. You do not need BeginTextureMode(), MPV sets up the correct openGL state itself.
+
+            mpv_render_context_render will block until a frame becomes available, essentially locking your game
+           framerate to the video framerate. See the advanced example for custom frame timings.
+        */
+        mpv_opengl_fbo mpv_fbo = {
+            .fbo = mpv_tex.id,
+            .w = screenWidth,
+            .h = screenHeight,
+        };
+        int flip_y = 0;
+        mpv_render_param render_params[] = {
+            {MPV_RENDER_PARAM_OPENGL_FBO, &mpv_fbo}, {MPV_RENDER_PARAM_FLIP_Y, &flip_y}, {0}};
+
+        mpv_render_context_render(mpv_render_ctx, render_params);
+
+        BeginDrawing();
+        // drawing
+        DrawTextureRec(mpv_tex.texture, (Rectangle){0.f, 0.f, screenWidth, screenHeight}, (Vector2){0, 0}, WHITE);
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+    /* Free the MPV resources before exiting */
+    UnloadRenderTexture(mpv_tex);
+    mpv_render_context_free(mpv_render_ctx);
+    mpv_destroy(mpv_ctx);
+    CloseWindow();
+    return 0;
 }
